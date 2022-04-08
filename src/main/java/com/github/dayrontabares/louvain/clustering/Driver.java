99c861dd23a6fb1030a8c37121462df61d2f4ee3
dtaba008@fiu.edu <dtaba008@fiu.edu>
2022-04-07 22:08:37 -0400

    second

--- src/main/java/com/github/dayrontabares/louvain/clustering/Driver.java
+++ src/main/java/com/github/dayrontabares/louvain/clustering/Driver.java
@@ -5,7 +5,7 @@ import com.github.neiljustice.louvain.graph.GraphBuilder;
 import com.github.neiljustice.louvain.nmi.NMI;
 
 
-public class driver {
+public class Driver {
 
     public static void main(String[] args) {
 
